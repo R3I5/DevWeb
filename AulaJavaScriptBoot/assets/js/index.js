19687e418e45b800831d411bb081818820cf8f21
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-09-02 11:37:24 -0300

    Aula JS BootStrap

---
+++ AulaJavaScriptBoot/assets/js/index.js
@@ -0,0 +1,36 @@
+function retornabotao(){
+    var nome;
+    var senha;
+    nome = document.getElementById("nome").value;
+    senha = document.getElementById("senha").value;
+
+    var retorno = baseFake(nome, senha);
+    var erros = [
+        "login efetudo com sucesso!",
+        "login certo, porém senha errada!",
+        "Login não encontrado"
+    ]
+    alert(erros[retorno])
+    }
+
+
+function baseFake(login, senha){
+    const baseFake = [];
+    baseFake.push({login: "foo", senha: "bla"})
+    baseFake.push({login: "teste", senha: "teste"})
+    baseFake.push({login: "joão", senha: "1234"})
+    baseFake.push({login: "fake", senha: "fake"})
+    baseFake.push({login: "fake_test", senha: "1234"})
+
+    for(var i=0; i<baseFake.length;i++){
+        var verify = baseFake[i];
+        if(login == verify.login){
+            if(senha == verify.senha){
+                return 0; //login e senha corretos
+            } else{
+                return 1; //login existe, senha errada
+            }
+        }
+    }
+    return 2; // só retorna 2 depois de verificar todos 
+}
